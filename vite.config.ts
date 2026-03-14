6cbffc41f930a82136387042d885ba3ea1a242e1
HouliJouli <112122102+HouliJouli@users.noreply.github.com>
2026-03-14 09:01:57 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  // Altere abaixo para o nome exato do seu repositório no GitHub
+  base: '/gdtool/',
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 2000,
+  },
+})
